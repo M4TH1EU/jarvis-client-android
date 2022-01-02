75bf1c3389df01d8c2b308b6d0acda4128b5b9f3
Mathieu <m4th1eu@protonmail.com>
2022-01-02 22:53:05 +0100

    Added footer, scrollable conversation and global UI improvements

--- app/src/main/java/ch/mathieubroillet/jarvis/android/MainActivity.kt
+++ app/src/main/java/ch/mathieubroillet/jarvis/android/MainActivity.kt
@@ -4,9 +4,7 @@ import android.os.Bundle
 import android.view.WindowManager
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
-import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.*
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
@@ -16,8 +14,6 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.font.Font
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -80,8 +76,31 @@ fun Base() {
 }
 
 @Composable
+fun Footer() {
+    //We create a row that we align to the bottom center of the parent box
+    Row(
+        Modifier
+            .padding(bottom = 50.dp)
+            .fillMaxWidth(),
+        verticalAlignment = Alignment.Bottom,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        //Microphone floating button to manually start/stop listening
+        FloatingActionButton(onClick = { /*TODO*/ }) {
+            Icon(
+                painter = painterResource(id = R.drawable.ic_baseline_mic_24),
+                contentDescription = "microphone"
+            )
+        }
+    }
+}
+
+@Composable
 fun MessageFromJarvis(text: String) {
+    //We create a row to contain the message and the robot image (to look like an sms)
     Row(Modifier.padding(bottom = 25.dp)) {
+
+        // Adding the robot image as the sender
         Image(
             painter = painterResource(id = R.drawable.robot256),
             contentDescription = "robot",
@@ -90,13 +109,13 @@ fun MessageFromJarvis(text: String) {
                 .padding(end = 10.dp)
         )
 
+        // Adding the message box with the text given in the params
         Box(
             modifier = Modifier
                 .fillMaxWidth(fraction = 0.9F)
                 .clip(RoundedCornerShape(15.dp))
                 .background(color = MaterialTheme.colors.secondaryVariant)
                 .padding(horizontal = 10.dp, vertical = 5.dp)
-
         ) {
             Text(text = text, fontFamily = productSansFont)
         }
@@ -105,10 +124,13 @@ fun MessageFromJarvis(text: String) {
 
 @Composable
 fun MessageFromUser(text: String) {
+    //We create a row to contain the user message and we align the row to the right side (to look like a conversation between two people)
     Row(
         Modifier
             .padding(bottom = 25.dp)
-            .fillMaxWidth(), horizontalArrangement = Arrangement.End) {
+            .fillMaxWidth(), horizontalArrangement = Arrangement.End
+    ) {
+        // The message box with the text
         Box(
             modifier = Modifier
                 .fillMaxWidth(fraction = 0.8F)
@@ -119,23 +141,48 @@ fun MessageFromUser(text: String) {
             Text(
                 text = text,
                 fontFamily = productSansFont,
-                color = if (!isSystemInDarkTheme()) Color.White else Color(15, 15, 15, 255))
+                color = if (!isSystemInDarkTheme()) Color.White else Color(15, 15, 15, 255)
+            )
         }
     }
-
-
 }
+
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     JarvisComposeTheme {
-        Column(Modifier.padding(top = 40.dp, start = 20.dp, end = 20.dp)) {
-            Base()
-            MessageFromJarvis(text = "Salut, je suis Jarvis! \nPose moi une question et je ferais de mon mieux pour te renseigner.")
-            MessageFromUser(text = "Quel temps fait-il à Paris en ce moment ?")
-            MessageFromJarvis(text = "A Paris, il fait actuellement 10 degrés et le ciel est nuageux.")
+        //We create a main box with basic padding to avoid having stuff too close to every side.
+        Box(
+            Modifier
+                .fillMaxHeight()
+                .fillMaxWidth()
+                .padding(horizontal = 20.dp)
+                .padding(top = 30.dp, bottom = 0.dp)
+        ) {
 
+            // This column regroup the base and all the conversations (everything except the footer)
+            Column {
+                Base()
+
+                // This column regroup only the conversations and make them scrollable
+                Column(
+                    modifier = Modifier
+                        .verticalScroll(rememberScrollState())
+                        .weight(weight = 1f, fill = false)
+                ) {
+                    // Basic interaction stuff for demo
+                    MessageFromJarvis(text = "Salut, je suis Jarvis! \nPose moi une question et je ferais de mon mieux pour te renseigner.")
+                    MessageFromUser(text = "Quel temps fait-il à Paris en ce moment ?")
+                    MessageFromJarvis(text = "A Paris, il fait actuellement 10 degrés et le ciel est nuageux.")
+                }
+            }
+
+
+            // Finally we add the footer to the bottom center of the main box
+            Column(Modifier.align(Alignment.BottomCenter)) {
+                Footer()
+            }
         }
     }
 }
